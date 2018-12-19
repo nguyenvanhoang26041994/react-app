5af319e434a1ed8464ed1d82fbc80d4cb0022085
HoangNguyen <hoangnv@nexlesoft.com>
2018-12-19 07:39:38 +0700

    Badge init

--- app/components/core.js
+++ app/components/core.js
@@ -1,6 +1,7 @@
 export { default as Avatar } from './Avatar';
 export { default as Alert } from './Alert';
 export { default as Button } from './Button';
+export { default as Badge } from './Badge';
 export { default as Checkbox } from './Checkbox';
 export { default as Divider } from './Divider';
 export { default as Dropdown } from './Dropdown';
