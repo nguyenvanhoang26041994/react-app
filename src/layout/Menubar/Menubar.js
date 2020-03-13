6989bbd70ca8e2e74a36dded19159169215bd506
Hoang Nguyen <nguyenvanhoang26041994@gmail.com>
2020-03-13 22:39:10 +0700

    neumorphism

--- src/layout/Menubar/Menubar.js
+++ src/layout/Menubar/Menubar.js
@@ -13,13 +13,13 @@ const Menubar = ({ className }) => {
     <div className="rc-menubar-wrapper">
       <div className={cn('rc-menubar', className)}>
         <User
-          className="ml-2"
+          className="ml-3"
           name="Nguyễn Văn Hoàng"
           avatarUrl="https://scontent-hkt1-1.xx.fbcdn.net/v/t1.0-1/p320x320/83290506_984513715279336_4057640429858848768_o.jpg?_nc_cat=101&_nc_sid=dbb9e7&_nc_ohc=RBZa-mAgWzkAX9PXXVp&_nc_ht=scontent-hkt1-1.xx&_nc_tp=6&oh=1bbbfc45c4eef77cad5eee9361ded2db&oe=5E926A7C"
         />
-        <Search className="ml-2" />
-        <TheRest className="ml-2" />
-        <SystemInfo className="mx-2" />
+        <Search className="ml-3" />
+        <TheRest className="ml-3" />
+        <SystemInfo className="mx-3" />
       </div>
     </div>
   )
