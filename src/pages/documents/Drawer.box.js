0e93aeab9fd40b9baaca87cfd6db2733dae0903b
Hoang Nguyen <nguyenvanhoang26041994@gmail.com>
2020-03-19 02:01:04 +0700

    Fix

--- src/pages/documents/Drawer.box.js
+++ src/pages/documents/Drawer.box.js
@@ -4,6 +4,8 @@ import { Drawer, PureDrawer, Button } from '../../components/core';
 import PracticeBox from '../../components/PracticeBox';
 import StepperBox from './Stepper.box';
 
+import TabsBox from './Tabs.box';
+
 export default ({ className }) => {
   const [isOpen, setIsOpen] = useState(false);
 
@@ -11,7 +13,8 @@ export default ({ className }) => {
     <div className={cn('flex flex-col', className)}>
       <PracticeBox className="flex flex-col" wrapperClassName="mb-2" header="DRAWER">
         <Drawer className="mb-2 w-1/2" header="Drawer Header" open={isOpen} onClose={() => setIsOpen(false)}>
-          <StepperBox hiddenHeader className="mt-2" />
+          <StepperBox className="mt-2" />
+          <TabsBox className="mt-2" />
         </Drawer>
         <div>
           <Button onClick={() => setIsOpen(true)}>Show Drawer</Button>
