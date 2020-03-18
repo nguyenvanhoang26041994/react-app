a8c788e870bbb15cb46f4ec31981c5bfcfc3b74b
Hoang Nguyen <nguyenvanhoang26041994@gmail.com>
2020-03-18 15:15:20 +0700

    OK

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -1,19 +1,62 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback, useRef, useMemo } from 'react';
 import cn from 'classnames';
 import PropTypes from 'prop-types';
 
+import Button from '../Button';
+import Icon from '../Icon';
+
 require('./Carousel.scss');
 
-const Carousel = ({ className }) => {
+const Carousel = ({ className, children }) => {
+  const [page, setPage] = useState(1);
+  const [body, setBody] = useState({ height: 0, width: 0 });
+  const [boxBody, setBoxBody] = useState({ height: 0, width: 0 });
+
+  const maxPage = useMemo(() => Math.ceil(boxBody.width / body.width), [body.width, boxBody.width]);
+
+  const ref = useRef();
+  const boxRef = useRef();
+
+  useEffect(() => {
+    setBoxBody({
+      height: boxRef.current.clientHeight,
+      width: boxRef.current.clientWidth,
+    });
+    setBody({
+      width: ref.current.clientWidth,
+    });
+  }, []);
+
+  const handleNext = useCallback(() => setPage(prev => {
+    const _page = prev + 1;
+    return _page > maxPage ? maxPage : _page;
+  }), []);
+  const handlePrev = useCallback(() => setPage(prev => {
+    const _page = prev - 1;
+    return _page < 1 ? 1 : _page;
+  }), []);
+
   return (
-    <div className={cn('rc-carousel', className)}>
-      <img
-        src="https://i.pinimg.com/originals/38/6f/47/386f47c88a7aaa497ec6edc1c02cc9b6.jpg"
+    <div
+      ref={ref}
+      className={cn('rc-carousel', className)}
+      style={{ height: boxBody.height }}
+    >
+      <div
+        ref={boxRef}
+        className="rc-carousel-box"
         style={{
-          width: '50%',
-          heigth: '50%',
+          left: -(page - 1) * body.width,
         }}
-      />
+      >
+        {children}
+      </div>
+      <Button className={cn('rc-carousel-prev', { '--hidden': page <= 1 })} circle transparent onClick={handlePrev}>
+        <Icon name="chevron-left" />
+      </Button>
+      <Button className={cn('rc-carousel-next', { '--hidden': page >= maxPage })} circle transparent onClick={handleNext}>
+        <Icon name="chevron-right" />
+      </Button>
     </div>
   );
 };
@@ -21,6 +64,7 @@ const Carousel = ({ className }) => {
 Carousel.displayName = 'Carousel';
 Carousel.propTypes = {
   className: PropTypes.string,
+  children: PropTypes.any,
 };
 Carousel.defaultProps = {};
 
--- src/pages/documents/Carousel.box.js
+++ src/pages/documents/Carousel.box.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import cn from 'classnames';
-import { Carousel } from '../../components/core';
+import { Carousel, Tooltip } from '../../components/core';
 import PracticeBox from '../../components/PracticeBox';
+
+const Item = ({ children }) => {
+  return (
+    <div className="neumorphism mx-2 flex justify-center items-center" style={{ height: 300, width: 200, borderRadius: '1em' }}>
+      {children}
+    </div>
+  );
+};
 
 export default ({ className }) => {
   return (
     <PracticeBox wrapperClassName={className} header="CAROUSEL">
       <Carousel>
-
+        <Item>{1}</Item>
+        <Item>{2}</Item>
+        <Item>{3}</Item>
+        <Item>{4}</Item>
+        <Item>{5}</Item>
+        <Item>{6}</Item>
+        <Item>{7}</Item>
+        <Item>{8}</Item>
+        <Item>{9}</Item>
+        <Item>{10}</Item>
       </Carousel>
     </PracticeBox>
   );
