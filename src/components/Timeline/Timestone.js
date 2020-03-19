14b7a64d05a6840c473ce7702913f1e526ca1a38
Hoang Nguyen <nguyenvanhoang26041994@gmail.com>
2020-03-19 14:47:14 +0700

    OK

--- src/components/Timeline/Timestone.js
+++ src/components/Timeline/Timestone.js
@@ -2,12 +2,18 @@ import React from 'react';
 import cn from 'classnames';
 import PropTypes from 'prop-types';
 
+import Icon from '../Icon';
+
 require('./Timestone.scss');
 
 const Timestone = ({ className, children, ...otherProps }) => {
   return (
     <div className={cn('rc-timestone', className)} {...otherProps}>
-      {children}
+      <div className="rc-timestone-rail" />
+      <Icon name="clock" className="rc-timestone-stone-icon"/>
+      <div className="rc-timestone-content">
+        {children}
+      </div>
     </div>
   );
 }
