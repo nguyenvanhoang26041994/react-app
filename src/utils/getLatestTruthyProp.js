6c494714d0e9dbf3b1f6b0f0e96fbf5acd1aee1c
Son Le <sontdt69@gmail.com>
2020-03-27 10:48:00 +0700

    Added utils function

---
+++ src/utils/getLatestTruthyProp.js
@@ -0,0 +1,15 @@
+export default (props = {}, deps = []) => {
+  if (props && deps.length > 0) {
+    const propsKey = Object.keys(props);
+    for (let i = propsKey.length - 1; i >= 0; i -= 1) {
+      for (let j = 0; j < deps.length; j += 1) {
+        if ((deps[j] === propsKey[i]) && props[propsKey[i]]) {
+          return {
+            [propsKey[i]]: props[propsKey[i]]
+          };
+        }
+      }
+    }
+  }
+  return false;
+};
