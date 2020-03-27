dee1336ab0b4a5cbdf9a8a05a9db7db5091a943d
Son Le <sontdt69@gmail.com>
2020-03-27 11:52:37 +0700

    Refactor utils function

--- src/utils/getLatestProp.js
+++ src/utils/getLatestProp.js
@@ -1,21 +1,24 @@
+import { isArray, isObject } from './helpers';
+
 /**
  * Find latest props based on dependencies.
  *
- * @param {object} props The input props.
- * @param {array} deps The array has value to find.
- * @return {object} The latest object value.
+ * @param {object} props - The input props.
+ * @param {array} deps - The array has value to find.
+ * @return {string} The latest key of value.
  */
 
 export default (props = {}, deps = []) => {
-  if (props && deps.length > 0) {
-    const propsKey = Object.keys(props);
-    for (let i = propsKey.length - 1; i >= 0; i -= 1) {
-      if (deps.indexOf(propsKey[i]) >= 0) {
-        return {
-          [propsKey[i]]: props[propsKey[i]]
-        };
-      }
+  const result = '';
+
+  if (!isObject(props) || !isArray(deps)) return result;
+
+  if (deps.length === 0) return result;
+
+  const propsKey = Object.keys(props);
+  for (let i = propsKey.length - 1; i >= 0; i -= 1) {
+    if (deps.indexOf(propsKey[i]) >= 0) {
+      return propsKey[i];
     }
   }
-  return {};
 };
