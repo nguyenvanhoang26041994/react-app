3a6596cfd783ab1b25a1ba01d3917838a2ab48be
Son Le <sontdt69@gmail.com>
2020-04-13 14:39:04 +0700

    Refactor

--- src/components/Loader/Spinner.js
+++ src/components/Loader/Spinner.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import cn from 'classnames';
+import PropTypes from 'prop-types';
 
-const Spinner = () => {
+const Spinner = ({ color, size, className }) => {
   return (
-    <></>
+    <div style={{ borderLeftColor: color, width: size, height: size }} className={cn('rc-loader-spinner', className)}>{''}</div>
   );
 };
 
 Spinner.displayName = 'Spinner';
-Spinner.propTypes = {};
+Spinner.propTypes = {
+  color: PropTypes.string,
+  size: PropTypes.string,
+  className: PropTypes.string,
+};
 Spinner.defaultProps = {};
 
 export default Spinner;
