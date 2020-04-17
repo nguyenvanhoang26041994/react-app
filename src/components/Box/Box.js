8883f60a73b18b9a974dff9742f8d328857abb52
Son Le <sontdt69@gmail.com>
2020-04-17 22:21:11 +0700

    Refactor Box component

--- src/components/Box/Box.js
+++ src/components/Box/Box.js
@@ -1,34 +1,26 @@
-import React, { useMemo } from 'react';
+import React from 'react';
 import cn from 'classnames';
 import PropTypes from 'prop-types';
 
 import Loader from '../Loader';
 
 const Box = ({ children, loading, skeleton, className, ...otherProps }) => {
-  const loaderContent = useMemo(() => {
-    if (loading) {
-      return (
-        <div className="rc-box-loader">
-          {skeleton ? skeleton : <Loader.Dot className="rc-box-dot" />}
-        </div>
-      );
-    }
-    return null;
-  }, [loading]);
-
   return (
-    <div className='rc-box'>
-      <div
-        className={cn(
-          'rc-box-content',
-          { '--loading': loading, '--hasSkeleton': loading && skeleton },
-          className
-        )}
-        {...otherProps}
-      >
+    <div
+      className={cn('rc-box', {
+        '--loading': loading,
+        '--skeleton': skeleton,
+        '--not-skeleton': !skeleton
+      })}
+    >
+      <div className={cn('rc-box-content', className)} {...otherProps}>
         {children}
       </div>
-      {loaderContent}
+      {loading && (
+        <div className="rc-box-loader">
+          {skeleton ? skeleton : <Loader.Dot />}
+        </div>
+      )}
     </div>
   );
 };
